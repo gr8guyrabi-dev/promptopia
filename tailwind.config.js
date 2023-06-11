0319cb50ae4480c8e7cf780b8505a717cdf81008
Rabi Maharjan <rabi.maharjan015@gmail.com>
2023-06-11 14:37:12 +0545

    feat: implement create new prompt feat.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
         inter: ['Inter', 'sans-serif'],
       },
       colors: {
-        'primary-orange': '£FF5722'
+        'primary-orange': '#FF5722'
       }
     },
   },
